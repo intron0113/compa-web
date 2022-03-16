6e6f6b8ccecfaedd2cabec320b127314f2d9829d
intron0113 <intron0113@gmail.com>
2022-03-17 00:14:49 +0900

    dev 3-17

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,44 @@
 <template>
-  <Tutorial/>
+  <div class="page"></div>
 </template>
 
 <script>
-export default {}
+export default {
+  async fetch({ store }) {
+    await store.dispatch('checkLogin')
+    store.dispatch('todo/getData')
+    store.dispatch('chat/getData')
+  },
+  computed: {
+    image() {
+      return this.$store.getters.user.photoURL
+    },
+    name() {
+      return this.$store.getters.user.name
+    },
+  },
+}
 </script>
+
+<style lang="scss" scoped>
+.page {
+  > .nav-header {
+    position: fixed;
+  }
+
+  > .container {
+    display: flex;
+
+    > .view-chat,
+    .view-task {
+      width: 50%;
+      position: fixed;
+      top: 40px;
+    }
+
+    > .view-task {
+      right: 0;
+    }
+  }
+}
+</style>
