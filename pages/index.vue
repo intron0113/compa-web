e144c892623cf295b4131a7a2e57f7bc2b9d7039
intron0113 <intron0113@gmail.com>
2022-03-22 13:49:27 +0900

    dev3-22

---
+++ pages/index.vue
@@ -0,0 +1,206 @@
+<template>
+  <div>
+    <span>
+      <!-- ナビゲーション -->
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        right
+        temporary
+        class="grey darken-4"
+      >
+        <v-list class="mx-auto">
+          <div v-if="!$store.state.isAuthenticated">
+            <v-btn class="white--text" text to="/login">ログイン</v-btn>
+            <v-btn text to="/register" class="register white--text">
+              ユーザー登録
+            </v-btn>
+          </div>
+          <div v-else>
+            <p>
+              <v-btn class="white--text" text to="/bookindex">ホーム</v-btn>
+            </p>
+            <p>
+              <v-btn class="white--text" text to="/search">投稿する</v-btn>
+            </p>
+            <p>
+              <v-btn class="white--text" text to="/profile">マイページ</v-btn>
+            </p>
+            <p>
+              <v-btn class="white--text" text @click="logout">ログアウト</v-btn>
+            </p>
+          </div>
+        </v-list>
+      </v-navigation-drawer>
+      <!-- ここまで -->
+
+      <v-app-bar app color="grey darken-4" dark class="nav" absolute temporary>
+        <!-- <v-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-icon> -->
+        <!-- <v-spacer class="hidden-md-and-up"></v-spacer> -->
+        <router-link to="/">
+          <v-toolbar-title>
+            {{ appTitle }}
+          </v-toolbar-title>
+        </router-link>
+
+        <!-- <v-spacer class="hidden-sm-and-down"></v-spacer> -->
+        <div class="hidden-md-and-up">
+          <!-- ヘッダーメニュー -->
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        </div>
+        <div v-if="!$store.state.isAuthenticated" class="hidden-sm-and-down">
+          <v-btn text to="/login">ログイン</v-btn>
+          <v-btn text to="/register" class="register"> ユーザー登録 </v-btn>
+        </div>
+        <div v-else class="hidden-sm-and-down">
+          <v-btn text to="/bookindex">ホーム</v-btn>
+          <v-btn text to="/search">投稿する</v-btn>
+          <v-btn text to="/profile">マイページ</v-btn>
+          <v-btn text @click="logout">ログアウト</v-btn>
+        </div>
+      </v-app-bar>
+    </span>
+    <section>
+      <v-container class="home"></v-container>
+      <v-main>
+        <div class="discription">
+          <h2>SHALIBOとは</h2>
+          <p class="discription__comment">
+            読書した内容を 1
+            ページにまとめるシンプルな「読書記録アプリ」です。<br />
+            本の内容をWEB
+            上で投稿・管理し、他のユーザーと知識を共有し合うことで、<br />
+            同じ本を要約することに時間をかけずに、まだ要約されていない書籍に専念することができます。<br />
+          </p>
+        </div>
+        <div class="features">
+          <h2>SHALIBOの特徴</h2>
+          <div class="features__list">
+            <div class="contents">
+              <p class="contents__title">アウトプット機能</p>
+              <div class="contents__discription">
+                <p>
+                  読み終えたら、読書した内容を質問テンプレートに沿って記録することができます。
+                </p>
+              </div>
+            </div>
+            <div class="contents">
+              <p class="contents__title">投稿記録</p>
+              <div class="contents__discription">
+                <p>
+                  読書した本の内容を記録。投稿した本はマイページに保存が残ります。
+                </p>
+              </div>
+            </div>
+            <div class="contents">
+              <p class="contents__title">編集機能</p>
+              <div class="contents__discription">
+                <p>コメント編集で文字の付け足しや、削除ができます。</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </v-main>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+.title {
+  width: 50%;
+  height: 20%;
+  padding-top: 30px;
+  text-align: center;
+  position: absolute;
+  top: 15%;
+  left: 25%;
+  background-color: rgba(255, 255, 255, 0.5);
+}
+.top__message {
+  width: 100%;
+  color: #232323;
+  font-size: 1rem;
+  line-height: 35px;
+  letter-spacing: 0.1rem;
+  text-align: center;
+}
+.sub__title {
+  font-size: 30px;
+}
+.discription {
+  width: 100%;
+  height: 150px;
+  color: #232323;
+  font-size: 1rem;
+  line-height: 35px;
+  letter-spacing: 0.1rem;
+  text-align: center;
+}
+.discription__comment {
+  font-size: 1rem;
+}
+.features {
+  width: 100%;
+  height: 360px;
+  margin: 50px 0 0 0;
+  padding: 48px 0 0 0;
+  text-align: center;
+  background-color: #f0e8df;
+}
+.features__list {
+  display: flex;
+  margin-top: 50px;
+}
+.contents {
+  width: 30%;
+  margin: 0 30px;
+}
+.contents__title {
+  border: solid 3px orange;
+  padding: 0.5em;
+  border-radius: 0.5em;
+}
+@media (max-width: 576px) {
+  .features {
+    width: 100%;
+    height: 550px;
+  }
+  .features__list {
+    flex-direction: column;
+    width: 100%;
+  }
+  .contents {
+    width: 80%;
+    margin: 0 auto;
+  }
+  .contents__discription {
+    font-size: 0.8rem;
+    margin-bottom: 30px;
+  }
+  .discription__comment {
+    font-size: 0.8rem;
+    padding: 10px;
+    line-height: 20px;
+  }
+}
+/* @media (min-width: px) {
+  .home {
+  background: url("~@/assets/mv.png");
+  background-blend-mode: lighten;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100%;
+  height: 55vh;
+  }
+} */
+.v-toolbar__content {
+  justify-content: space-between;
+}
+</style>
