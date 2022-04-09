0617fc5fd0af7d6c1c03037ff5015b9d6a468ed5
intron0113 <intron0113@gmail.com>
2022-04-10 08:30:14 +0900

    dev-4-10

--- components/templates/NavHeader.vue
+++ components/templates/NavHeader.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="nav nav-header">
-    <h1 class="title">WeAre</h1>
+  <div>
     <IconUser :image="image" />
   </div>
 </template>
@@ -15,21 +14,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.nav-header {
-  padding: 0 20px 0 18px;
-  height: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-bottom: 1px solid #bbbbbb;
-  width: 100%;
-
-  > .title {
-    font-family: "Pacifico", cursive;
-    font-size: 22px;
-    line-height: 120%;
-  }
-}
-</style>
-s
+<style lang="scss" scoped></style>
