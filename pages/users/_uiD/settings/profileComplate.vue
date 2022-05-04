90852c81ce2437ca252cf0b0f180502f0c07b0ce
intron0113 <intron0113@gmail.com>
2022-05-04 18:10:30 +0900

    dev-5-4

---
+++ pages/users/_uiD/settings/profileComplate.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col
+        class="
+          col-lg-8
+          col-lg-offset-2
+          col-md-8
+          col-md-offset-2
+          col-sm-10
+          col-sm-offset-1
+        "
+      >
+        <div>
+          <div class="text-center">プロフィール登録</div>
+        </div>
+        <hr class="mb-10" />
+        <div class="row" align="center">
+          <div class="col-sm-12 sessionsLayoutFormColumn">
+            <div>登録完了しました</div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row class="button">
+      <v-col cols="12">
+        <v-btn to="/" color="primary" depressed elevation="2" large>登録</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+// トップページではヘッダーを表示しないレイアウトを使用
+export default {
+  // レイアウト plainを指定
+  layout: 'plain',
+}
+</script>
+<style>
+.button {
+  text-align: center;
+  margin: 30px 0;
+}
+</style>
