cea9fd15edf351f12df7490b6ecd15e3b2ef6e8a
intron0113 <intron0113@gmail.com>
2022-05-29 00:18:39 +0900

    dev 5-29

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,9 +7,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nuxt-app",
+    title: "Co-mpa",
     htmlAttrs: {
-      lang: "en",
+      lang: "ja",
     },
     meta: [
       { charset: "utf-8" },
